eeb4e4ba8dffba3f553c9873d972680ff56b1f55
invalid1 <weirdestnerd@gmail.com>
2021-02-22 15:25:24 -0300

    Changed how theme is installed

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,6 @@ release = '1.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -54,3 +53,4 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_theme_path = ["_themes",]
